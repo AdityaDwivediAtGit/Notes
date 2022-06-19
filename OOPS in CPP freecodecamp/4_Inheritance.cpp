71a3554429306aa64f406cca3cfe12ca9a30701b
AdityaDwivediAtGit <107645490+AdityaDwivediAtGit@users.noreply.github.com>
2022-06-20 00:25:46 +0530

    Before starting Protected Access Modifier

--- OOPS in CPP freecodecamp/4_Inheritance.cpp
+++ OOPS in CPP freecodecamp/4_Inheritance.cpp
@@ -15,7 +15,10 @@ since developer class is derived from employee class, so default constructor wil
 Also, since all the variables of class Employee are copied to class Developer,
 The constructor of Developer will also contain variables inside the employee class)
 note : we dont have to specify type in the constructor of developer class when referencing to employee
+3. Provided the favourateprogrammingLanguage string to provide objects d1 and d2 to Developer Class in main()
+4. Created a method called PrintProgLangMethod() to check if 
 */
+
 
 
 #include <iostream>
@@ -26,6 +29,7 @@ class AbstractEmployee{
     virtual void AskForPromotion()=0;   // Rule : making mandatory
 };
 
+
 // class "Employee" (developer) signing the contract "AbstractEmployee" (user end)
 //class company: contract
 class Employee:AbstractEmployee{
@@ -35,6 +39,29 @@ private:
     int Age;
 
 public:
+
+// Setters and Getters
+    void setName_123(string name){
+        Name = name;
+    }
+    void setComp_123(string company){
+        Company = company;
+    }
+    void setAge__123(int age){
+        if (age>=18)
+            Age = age;
+    }
+
+    string getName_123(){
+        return Name;
+    }
+    string getComp_123(){
+        return Company;
+    }
+    int getAge__123(){
+        return Age;
+    }
+
     // Constructor
     Employee(string name, string company, int age)
     {
@@ -52,22 +79,32 @@ public:
     }
 };
 
+
 // Creating a derived class for Inheritance
 // class [Derived class] : [Base class]
 class Developer:Employee{
     // All the Attributes (Methods, veriables, etc.) from "Employee" are imported here.
 public:
-    string s;
+    string FavouriteProgrammingLanguage;
 
-                               
     Developer(string name, string company, int age, string s1):Employee(name, company, age){
         // Note :                   we have not specified datatype here ^^^^^^^^^^^^^^^^^^
-        s=s1;
+        FavouriteProgrammingLanguage=s1;
+    }
+
+    // Creating a Method to print the details of developers d1 and d2
+    void PrintFavProgLangMethod(){
+        // Printing using getter method, since private variable Name of Employee Class is not directly Accessible to Developer class.
+        cout<<"The favourate Programming language of "<<getName_123()<<" is "<<FavouriteProgrammingLanguage<<endl;
     }
 };
 
 int main()
 {
-    Developer d1 = Developer("Aditya Dwivedi", "RCOEM", 21);
-    Developer d2 = Developer("Saldina Nurak", "freecodecamp and CodeBeauty", 25);
+    Developer d1 = Developer("Aditya Dwivedi", "RCOEM", 21, "Python");
+    Developer d2 = Developer("Saldina Nurak", "freecodecamp and CodeBeauty", 25, "C++");
+    d1.PrintFavProgLangMethod();
+    d2.PrintFavProgLangMethod();
 }
+
+// Completed till 1:06:00
